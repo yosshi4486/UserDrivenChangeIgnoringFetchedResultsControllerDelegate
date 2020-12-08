2a41c45f6d67f87c6954e8a40444dc47b20f5646
yosshi4486 <yoshiharuseijin@gmail.com>
2020-12-08 18:31:25 +0900

    feat: Add Configuration(Configurator).

---
+++ Sources/UserDrivenChangeIgnoringFetchedResultsControllerDelegate/CellConfiguration.swift
@@ -0,0 +1,18 @@
+//
+//  CellConfiguration.swift
+//  UserDrivenChangeIgnoringFetchedResultsControllerDelegate
+//
+//  Created by yosshi4486 on 2020/12/08.
+//
+
+#if canImport(UIKit) || targetEnvironment(macCatalyst)
+
+import UIKit
+
+public protocol CellConfiguration {
+
+    func configure(_ cell: UITableViewCell, at indexPath: IndexPath)
+
+}
+
+#endif
